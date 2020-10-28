abfbcd7e4de992af22da0339e5e5a0aca4915c09
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-10-27 20:41:44 -0700

    credentials

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: process.env.MONGO_URI,
-    PORT: process.env.PORT || 3000,
-}
+  mongoUri: "mongodb+srv://admin:9URteDDd@cluster0.rbwmc.mongodb.net/MEVN?retryWrites=true&w=majority",
+  PORT: process.env.PORT || 3000,
+};
