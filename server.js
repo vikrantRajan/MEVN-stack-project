f675d60a33605446514ff24b221ac2595647a906
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2020-10-27 19:31:25 -0700

    Prepared to deploy to heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const bucketListItemRoutes = require("./routes/api/bucketListItems");
+const path = require('path');
 
 app.use(cors());
 app.use(morgan("tiny"));
@@ -22,7 +23,13 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/bucketListItems", bucketListItemRoutes);
-app.get("/", (req, res) => res.send("Hello World"));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static("checklist-app/dist"))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "checklist-app", 'dist', 'index.html'));
+  })
+}
 
 app.listen(PORT, () =>
   console.log(`App listening at http:\\localhost:${PORT}`)
